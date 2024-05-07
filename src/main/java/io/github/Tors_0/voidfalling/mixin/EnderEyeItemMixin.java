dbb4bf3dda2cbf5f0d7f98b6105ee09115f0eef4
rae <raej0000@gmail.com>
2024-05-06 19:58:14 -0700

    SURPRISE! I HIJACKED THE END PORTAL!

---
+++ src/main/java/io/github/Tors_0/voidfalling/mixin/EnderEyeItemMixin.java
@@ -0,0 +1,26 @@
+package io.github.Tors_0.voidfalling.mixin;
+
+import io.github.Tors_0.voidfalling.registry.ModBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.pattern.BlockPattern;
+import net.minecraft.item.EnderEyeItem;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(EnderEyeItem.class)
+public abstract class EnderEyeItemMixin {
+	@Inject(method = "useOnBlock", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/block/pattern/BlockPattern$Result;getFrontTopLeft()Lnet/minecraft/util/math/BlockPos;"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
+	private void voidfalling$hijackTheEndPortal(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir, World world, BlockPos blockPos, BlockState blockState, BlockState blockState2, BlockPattern.Result result) {
+		BlockPos centerBlockPos = result.getFrontTopLeft().add(-2, 0, -2);
+		if (world.getBlockState(centerBlockPos).isOf(ModBlocks.VOIDED_BLOCK)) {
+			cir.setReturnValue(ActionResult.CONSUME); // YES IT WORKS
+		}
+	}
+}
